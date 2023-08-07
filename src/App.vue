231675411022133c5af963cd218b9387d3296053
calm1205 <naginani072@gmail.com>
2023-08-07 09:06:38 +0900

    sample Line

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts">
 import HelloWorld from "./components/HelloWorld.vue";
+import Line from "./components/Line.vue";
 </script>
 
 <template>
   <HelloWorld msg="Vite + Vue" />
+  <Line />
 </template>
 
 <style scoped></style>
---
+++ src/components/Line.vue
@@ -0,0 +1,45 @@
+<script setup lang="ts">
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend,
+} from "chart.js";
+import { Line } from "vue-chartjs";
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend
+);
+
+const data = {
+  labels: ["January", "February", "March", "April", "May", "June", "July"],
+  datasets: [
+    {
+      label: "貯金",
+      backgroundColor: "#f87979",
+      data: [40, 39, 10, 40, 39, 80, 40],
+    },
+    {
+      label: "積立",
+      backgroundColor: "black",
+      data: [39, 10, 40, 80, 40, 21, 57],
+    },
+  ],
+};
+</script>
+
+<template>
+  <Line :data="data" />
+</template>
+
+<style scoped></style>
